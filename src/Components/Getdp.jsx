b791f2e5cf88f773fbbdf55c1b2e8d9f7a1cb3f7
Devtimmy <oyinloyematthew6@gmail.com>
2025-05-10 00:20:20 +0100

    adjusted the mobile layout

--- src/Components/Getdp.jsx
+++ src/Components/Getdp.jsx
@@ -10,7 +10,7 @@ const Getdp = () => {
       <div className="py-8 px-4 md:px-12 lg:px-24">
           
           {/* Navbar section */}
-          <div className="mb-24 border-b border-neutral-700/80 pb-8">
+          <div className="mb-16 border-b border-neutral-700/80 pb-8">
               <nav className="w-full">
                   <ul className="flex flex-row items-center justify-center gap-6 md:gap-12 ">
                       <li className="home cursor-pointer hover:text-yellow-600 transition">Home</li>
@@ -24,7 +24,7 @@ const Getdp = () => {
           </div>
 
           {/* Main section */}
-          <div className="getdp-section py-12 px-8 space-y-8 mb-24 ">
+          <div className="getdp-section py-2 px-2 md:py-12 md:px-8  lg:py-12 lg:px-8 space-y-8 mb-16 ">
               <div className="text-center mb-8 flex justify-center flex-col items-center">
                 <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-yellow-600 mb-4">Get Your Chasers Conference Display Picture</h2>
                 <p className="text-base max-w-2xl mx-auto text-neutral-300"> Kindly fill in the form below to get your Chasers DP
@@ -32,7 +32,7 @@ const Getdp = () => {
               </div>
               
               {/* Flier Section */}
-              <div className="form-flier-container bg-[#1f112d] shadow-lg rounded-lg px-12 py-16 flex flex-col justify-center items-center">
+              <div className="form-flier-container bg-[#1f112d] shadow-lg rounded-lg px-4 py-8 md:px-12 md:py-12 lg:px-12 lg:py-12 flex flex-col justify-center items-center">
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12 mb-16 ">
                       {/* Flier container */}
                     <div className="graphics flex justify-center items-center bg-[#2A1A3A] shadow-lg rounded-lg p-6 h-full ">
@@ -40,7 +40,7 @@ const Getdp = () => {
                         </div>
 
                         {/* Form Input section */}
-                      <div className="p-6 h-full">
+                      <div className="p-2 h-full">
                           
                             <form action="" className="flex flex-col space-y-16 w-full" method="POST">
                                 <div className="flex flex-col space-y-2">
@@ -61,8 +61,8 @@ const Getdp = () => {
 
                   {/* Download Button */}
 
-                    <div className="text-center bg-yellow-600 py-4 px-6 rounded-lg hover:bg-yellow-700 hover:scale-105 transition-transform transform">
-                        <button className="download btn">Download Your Chasers-DP</button>
+                    <div className="text-center bg-yellow-600 py-4 px-2 md:px-6 lg:px-6 rounded-lg hover:bg-yellow-700 hover:scale-105 transition-transform transform">
+                        <button className="download font-semibold">Download Your Chasers-DP</button>
                     </div>
 
               </div>
@@ -74,7 +74,7 @@ const Getdp = () => {
 
           {/* Footer Section */}
           
-          <div className="footer flex flex-col gap-8 justify-center items-center border-t border-neutral-700/80 py-24">
+          <div className="footer flex flex-col text-center gap-8 justify-center items-center border-t border-neutral-700/80 py-24">
               <div className="flex gap-4 ">
                   
                 <Facebook color="white" strokeWidth={0.75} />
