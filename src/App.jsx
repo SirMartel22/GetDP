24f4d48102d848715902805a7f052efdbfb62c7f
Devtimmy <oyinloyematthew6@gmail.com>
2025-05-09 01:26:40 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import Getdp from './Components/Getdp'
 
 function App() {
 
   return (
     <>
-    
+     <Getdp />
     </>
   )
 }
---
+++ src/Components/Getdp.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from '/logo1.png'
+
+const Getdp = () => {
+  return (
+      <div>
+          
+          {/* Navbar section */}
+          <div className="nav">
+              <nav className="">
+                  <ul className="">
+                      <li className="home">Home</li>
+                      <li className="logo">
+                          <img src={ logo } />
+                      </li>
+                      <li className="home">Get Chasers DP</li>
+                  </ul>
+                  
+              </nav>
+          </div>
+
+          {/* Main section */}
+          <div className="getdp-section ">
+              
+            {/* Flier Section */}
+              <div className="graphics">
+                  <img />
+              </div>
+
+            {/* Form Input section */}
+            <div className="form">
+                  <form action="" className="" methof="POST">
+                      
+                      <input name="name" type="text" />
+                      <input name="image" type="file/image"/>
+                  </form>
+            </div>
+
+          </div>
+
+          {/* Download Button */}
+          <div>
+              <button className="download">Download Your Chasers-DP</button>
+          </div>
+
+
+          {/* Footer Section */}
+          
+          <div className="footer">
+              
+          </div>
+
+      
+    </div>
+  );
+}
+
+export default Getdp;
